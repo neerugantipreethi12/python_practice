05c77e53bdc7db9dce337ebe337628be86e46852
neerugantipreethi12 <neerugantipreethi2@gmail.com>
2026-03-09 19:41:46 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,34 @@
+
+#PYTHON  DAY1 PRATICE
+print("HELLO WORLD!")
+print('''welocome to python''')
+print(10+5)
+print("Day 1 of coding")
+#varaibles .SPLIT() METHOD
+name="preethi,hyderabad"
+city= name.split(",")
+print(city )
+#JOIN()
+words=("python","is","fun")
+result="*".join(words)
+print(result)
+#SEPARATOR
+print("preethi","Neeruganti","MCA",sep=" ,")
+#ACCESSING INDIVIDUAL PARTS
+tx ="java oops"
+words=tx.split()
+print(words[0])
+print(words[1])
+#float
+price=99.5
+print(price)
+#boolean
+#is_learn = True
+#print(is_learn)
+#Loop Through Split String
+text="learn python programming"
+words=text.split()
+
+for word in words:
+    print(word) 
+

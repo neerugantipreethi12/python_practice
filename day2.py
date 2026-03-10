1765aba25b2cc8774f7a18101e8dafe4e7f5da4e
neerugantipreethi12 <neerugantipreethi2@gmail.com>
2026-03-10 18:41:20 +0530

    Add files via upload
    day2 python practice

---
+++ day2.py
@@ -0,0 +1,43 @@
+#TYPECASTING
+num="10"
+print(int(num))
+x=5
+print(float(x))
+y=20
+print(str(y))
+a="15"
+b=5
+print(int(a)+b)
+num=7
+print(float(num))
+price=99.5
+print(int(price))
+num=10
+print(str(num))
+a=10
+b=56
+print(a+b)
+name=input("Enter your name:")
+print(name)
+
+#IF STATEMENTS
+age = int(input("enter the age: "))
+if age >= 18:
+   print("eligible for vote")
+else:
+   print("not eligilable for vote") 
+
+sal=5000
+if sal > 300:
+  print("good salary")
+
+temperature =40
+if temperature > 30:
+  print("HOT")
+else:
+ print("cool")   
+num = 7
+if num % 2 == 0:
+    print("EVEN")
+else:
+    print("ODD")    

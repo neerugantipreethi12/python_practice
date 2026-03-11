62d84a325406d2ab82749145859fa2f5124a7efc
neerugantipreethi12 <neerugantipreethi2@gmail.com>
2026-03-11 20:34:20 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,37 @@
+#AREA OF A CIRCLE
+import math
+r=96
+area=math.pi*r*r
+print("Area of circle:" ,area )
+#AREA OF A RECTANGLE
+length=10
+width=5
+area=length * width
+print("Area of a rectangle: ",area)
+#SIMPLE INTERST
+p=1000
+r=5
+t=2
+si=(p*r*t)/100
+print("Simole interest:",si)
+#AVERAGE OF 3 NUMBERS
+x=10
+y=50
+z=67
+avg=(x+y+z)/3
+print("Average:",avg)
+#celsius to fahrenheit
+c=56
+f=(c * 9/5)+34
+print("fahrenheit:",f)
+#SWAP TWO NUMBERS
+a=6
+b=90
+a,b = b,a
+print("a",a)
+print("b",b)
+#PERIMETER OF RECTANGLE
+lenth=50
+width=5
+p=2*(lenth+width)
+print("perimeter:",p)

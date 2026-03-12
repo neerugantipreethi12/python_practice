c8ea305170da1d64925a4627097b6678adb3cc05
neerugantipreethi12 <neerugantipreethi2@gmail.com>
2026-03-12 15:00:09 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,57 @@
+# IN PYTHON OPERATORS
+#ARITHMETIC OPERATORS
+#ADDITION
+#a=10
+#b=56
+#print(a+b)
+#subtraction
+#x=454
+#y=5
+#print(x-y)
+#multiplication
+#pree=56
+#sr=5
+#print(pree*sr)
+#division
+#div=45
+#vid=7
+#print(div/vid)
+#modulus
+#mod=4.5
+#od=6
+#print(mod%od)
+#floor
+#ty=45
+#yt=5
+#print(ty//yt)
+#EXPONENT
+#rrr=45
+#ww=8
+#print(rrr**ww)
+#comparsion
+#we=34
+#qw=34
+#print(we==qw)
+#er=4
+#te=5
+#print(er!=te)
+dsa=40
+sdp=10
+print(dsa>sdp)
+#check even or odd
+num=8
+print(num%2==0)
+#check greater number
+a=10
+b=20
+print(a>b)
+#LOGICAL CONDITION
+x=15
+print(x>10 and x<20)
+#MEMBERSHIP
+name="preethi"
+print("p" in name)
+#ASSIGMENT EXAMPLE
+x=5
+x +=10
+print(x)

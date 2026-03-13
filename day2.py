c01e27dbad78373eb9d5c874721f25f4ba9923db
neerugantipreethi12 <neerugantipreethi2@gmail.com>
2026-03-13 20:37:39 +0530

    Add files via upload

--- day2.py
+++ day2.py
@@ -1,43 +1,68 @@
-#TYPECASTING
-num="10"
-print(int(num))
-x=5
-print(float(x))
-y=20
-print(str(y))
-a="15"
-b=5
-print(int(a)+b)
-num=7
-print(float(num))
-price=99.5
-print(int(price))
-num=10
-print(str(num))
-a=10
-b=56
-print(a+b)
-name=input("Enter your name:")
-print(name)
+#LOOPS examples
+for i in range(1, 11):
+    print(i)
 
-#IF STATEMENTS
-age = int(input("enter the age: "))
-if age >= 18:
-   print("eligible for vote")
-else:
-   print("not eligilable for vote") 
+#Print Even Numbers 1 to 20
 
-sal=5000
-if sal > 300:
-  print("good salary")
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+#Multiplication Table
 
-temperature =40
-if temperature > 30:
-  print("HOT")
-else:
- print("cool")   
-num = 7
-if num % 2 == 0:
-    print("EVEN")
-else:
-    print("ODD")    
+num = int(input("Enter number: "))
+
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)
+ Sum of Numbers from 1 to N
+
+n = int(input("Enter number: "))
+sum = 0
+
+for i in range(1, n+1):
+    sum = sum + i
+
+print("Sum =", sum)
+#While Loop Examples
+Print Numbers 1 to 5
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+ #Reverse Counting
+
+i = 10
+
+while i >= 1:
+    print(i)
+    i -= 1
+#Sum Until User Enters 0
+
+total = 0
+num = int(input("Enter number (0 to stop): "))
+
+while num != 0:
+    total += num
+    num = int(input("Enter number (0 to stop): "))
+
+print("Total =", total)
+# Break Example
+ Stop Loop When Number is 5
+
+for i in range(1, 10):
+    if i == 5:
+        break
+    print(i)
+#Continue Example
+Skip Number 5
+
+for i in range(1, 10):
+    if i == 5:
+        continue
+    print(i)
+#Pass Example
+ Pass Statement
+
+for i in range(5):
+    pass

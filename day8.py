77d0d44b3c5ef46cf9352b93ddcd05be727c6038
neerugantipreethi12 <neerugantipreethi2@gmail.com>
2026-03-14 22:24:29 +0530

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,40 @@
+# for i in range(1,11):
+    # print(i)
+#SWAPPING OF TWO VARAIBLES
+# a=5
+# b=10
+# temp =a
+# a=b
+# b=temp   
+# print(a,b)/
+# EXAMPLE 2
+#x=5
+#y=6
+#x,y=y,x
+# print(x,y) 
+# PRIME NUMBERS
+# num=56
+# count = 0
+# for i in range(1,num+1):
+    # if num % i==0:
+        # count+=1
+# if count ==2:
+    # print("Prime number") 
+# else:
+    # print("Not a prime number")            
+        # FABONACCI SERIES
+# n=10
+# a=0
+# b=1
+# for i in range(n):
+        # print(a)
+        # c = a+b
+        # a =b
+        # b=c
+# ASTERISK PATTERN PRACTICE
+for i in range(6,0,-1):
+ print("*"*i)
+#  TRIANGLE
+for i in range(1,6):
+ print(" "*(5-i) + "*"*(2* i-1))
+

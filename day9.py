893195df9bd4f444401007db4986b4d77094849e
neerugantipreethi12 <neerugantipreethi2@gmail.com>
2026-03-15 18:05:34 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,42 @@
+# FOR LOOP ASTERISK
+# for i in range(1,11):
+    # print("*" * i)
+# USING AND OPERATOR
+# a = int(input("Enter value:" ))
+# b = int(input("Enter value:"))
+# if a > 9 and b >=40:
+    # print("both conditions are true")
+# else: 
+    # print("both conditions are false") 
+# VOWEL CHECK PROGRAM
+# letter = input("Enter a letter:")
+# if letter in "aeiouAEIOU":
+    # print("It is a vowel")
+# else:
+    # print("It is a consonant") 
+#EXPRESSION EXAMPLE
+# x = 3
+# b =5
+# result =(x+b)*2
+# print(result)      
+#PRINT EVEN NUMBERS
+# for i in range(2,15,2):
+    # print(i) 
+#Factorial program
+num = 9
+fact = 1
+for i in range(1,num +1):
+    fact = fact *i
+    print("Factorial:", fact) 
+#REVERSE STAR PATTERN
+for i in range(5,0,-1):
+    print("*" * i) 
+#count EXAMPLE
+text = "python"
+count =0
+for i in text:
+    count+=1
+    print(count)       
+
+
+
